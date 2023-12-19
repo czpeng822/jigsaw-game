fb75ccec58ce33450c0779ec685030388a468a43
czpeng822 <153256821+czpeng822@users.noreply.github.com>
2023-12-19 16:08:12 -0500

    Update jigsaw.js

--- js/jigsaw.js
+++ js/jigsaw.js
@@ -1,23 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var images = document.querySelectorAll('.image, .img1, .img2');
+var images = document.querySelectorAll('.image, .img1, .img2');
 
-    images.forEach(function(image) {
+/*    images.forEach(function(image) {
         image.addEventListener('click', function(event) {
             event.target.style.display = 'none';
             console.log('Image hidden');
         });
-    });
+    });   */
 
-    function reset() {
-        var image = document.querySelector('.image');
-        var img1 = document.querySelector('.img1');
-        var img2 = document.querySelector('.img2');
+for (var i=0;i<3;i++){
+    images[i].addEventListener('click', function(event) {
+        event.target.style.display = 'none';
+        console.log('Image hidden');
+    });
+}
+
+function reset() {
+    var image = document.querySelector('.image');
+    var img1 = document.querySelector('.img1');
+    var img2 = document.querySelector('.img2');
 
         image.style.display = 'block';
         img1.style.display = 'flex';
         img2.style.display = 'flex';
         console.log('reset success');
     }
-let btn=document.getElementById('reset')
-btn.onclick=reset;
-})
+let btn=document.getElementById('reset');
+btn.onclick=reset
